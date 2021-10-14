587ab1edb248c49798ee66f1ee00e3a50aee5a01
John Patrick Amata <johnamata97@gmail.com>
2021-10-15 02:34:25 +0800

    relative search

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ function Header() {
             <div className="flex justify-between max-w-6xl">
                 {/* left hand side to put the instagram logo*/}
                 {/* tailwind is mobile first, so if i put in "hidden" for classname it'll hide the image on small screen, the lg:inline-grid is for it to appear on large screen*/}
-                <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+                <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
                     {/* relative :: relative to parent size*/}
                     {/* cursor-pointer :: so we can hover it and have a cursor turn to pointing like it's a link*/}
                     <Image src="https://johnamata.github.io/siteassets/instagram-clone/ig.webp" 
@@ -25,7 +25,7 @@ function Header() {
                 </div>
                 {/* comment*/}
                 {/* w-10 :: width of 10, h-10 :: height of 10*/}
-                <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+                <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
                     {/* lg:hidden means on large screen it becomes hidden, basically opposite of the instagram logo so that this one only appears to "replace" the larger instagram logo when the screen is smaller*/}
                     {/* flex-shrink-0 :: as screen gets smaller, i dont want this logo to be smaller*/}
                     <Image src="https://johnamata.github.io/siteassets/instagram-clone/insta-logo.png" 
@@ -35,10 +35,14 @@ function Header() {
                 </div>
 
                 {/* middle where i put the search bar*/}
-                <div>
-                    {/* comment*/}
-                    {/* using heroicons from tailwindcss https://heroicons.com/ which need to be npm installed */}
-                    <SearchIcon className="h-5 w-5 text-gray-500"/> 
+                {/* pointer-events-none :: so nothing wont happen to our pointer*/}
+                <div className="relative mt-1 p-3 rounded-md bg-red-300">
+                    {/* relative to position according to the container*/}
+                    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                        {/* comment*/}
+                        {/* using heroicons from tailwindcss https://heroicons.com/ which need to be npm    installed */}
+                        <SearchIcon className="h-5 w-5 text-gray-500"/> 
+                    </div>
                     <input type="text" placeholder="search" />
                 </div>
                 {/* right hand side where i put the profiles/modals */}
