81878e513c3bd89875cd1f910d659eabaab8c42d
John Patrick Amata <johnamata97@gmail.com>
2021-10-16 02:31:51 +0800

    correct centering, fix max-auto to mx-auto

--- components/Feed.js
+++ components/Feed.js
@@ -2,7 +2,7 @@ import Stories from "./Stories"
 
 function Feed() {
     return (
-        <main class="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto">
+        <main class="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
             {/* comment */}
             {/* grid grid-cols-1 :: by default on mobile it'll be 1 column */}
             
